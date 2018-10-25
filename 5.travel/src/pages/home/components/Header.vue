596f4d22089eda4d498187e0716c380213e38bfa
rujin_shi <rujin_shi@163.com>
2018-10-25 08:39:24 +0800

    增加iconfont

--- 5.travel/src/pages/home/components/Header.vue
+++ 5.travel/src/pages/home/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="header">
-    <div class="hader-left">返回</div>
-    <div class="hader-input">输入城市/内容/游玩主题</div>
-    <div class="hader-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe6aa;</span>
+    </div>
 </div>
 </template>
 
@@ -13,24 +20,27 @@ export default {
 </script>
 <style scoped lang="stylus">
   .header
-    display: flex;;
-    line-height: .86rem;
-    background: #00bcd4;
-    color: #fff;
+    display flex
+    line-height .86rem
+    background #00bcd4
+    color #fff
     .header-left
-      width: .64rem;
-      float: left;
+      width .64rem
+      float left
+      .back-icon
+        text-align center
+        font-size .4rem
     .header-input
-      flex: 1;
+      flex 1
       height .64rem
       line-height .64rem
       margin-top .12rem
       margin-left .2rem
-      background: #fff;
-      border-radius: .1rem;
+      background #fff
+      border-radius .1rem
       color #ccc
     .header-right
-      width: 1.24rem;
-      float: right;
-      text-align: center;
+      width 1.24rem
+      float right
+      text-align center
 </style>
