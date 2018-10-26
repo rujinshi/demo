4b0f6b644025c4ec5ca6c26ba7a7eed94132e018
rujin_shi <rujin_shi@163.com>
2018-10-26 20:58:44 +0800

    index finish

--- 5.travel/src/pages/home/components/Swiper.vue
+++ 5.travel/src/pages/home/components/Swiper.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
-      <swiper-slide v-for="item of swiperList" :key="item.id">
+    <!-- v-if的作用是避免轮播图不是从第一章开始轮播 -->
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl">
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -12,36 +13,34 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '001',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
-      }, {
-        id: '002',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/d3/51a22fafb250e502.jpg_750x200_e4d9c5ae.jpg'
-      }, {
-        id: '003',
-        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
 </script>
 <style lang="stylus" scoped>
   .wrapper >>>.swiper-pagination-bullet-active
-    background #fff !important
+    background rgba(255,255,255,.5)
   .wrapper
     overflow hidden
     width 100%
     height 0
     // 使用百分比无论哪个方向 参照父容器的宽度 实现高度自适应
     // 用于占位 避免闪烁
-    padding-bottom 26.3%
+    padding-bottom 30%
     background #eee
     .swiper-img
       width 100%
