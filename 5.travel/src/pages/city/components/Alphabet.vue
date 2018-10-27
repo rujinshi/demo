946e7400ec8e057ea3e2a6d715194caf19f1216c
rujin_shi <rujin_shi@163.com>
2018-10-27 11:06:03 +0800

    finish city css

---
+++ 5.travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    // 弹性盒布局 用于垂直居中
+    display flex
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+
+    .item
+      line-height .4rem
+      // 控制字母水平居中
+      text-align center
+      color  $bgColor
+</style>
