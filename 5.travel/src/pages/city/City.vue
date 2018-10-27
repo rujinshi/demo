0af7b2b2a0cbb1a5ae021dc7cdb391869168647a
rujin_shi <rujin_shi@163.com>
2018-10-27 17:26:12 +0800

    finfish search

--- 5.travel/src/pages/city/City.vue
+++ 5.travel/src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <!-- 向子组件传递值 -->
     <city-list
     :cities="cities"
--- 5.travel/src/pages/city/components/Search.vue
+++ 5.travel/src/pages/city/components/Search.vue
@@ -1,11 +1,69 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市或拼音">
+  <div>
+    <div class="search">
+      <!-- 双向绑定 输入值 -->
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市或拼音">
+    </div>
+  <div class="search-content" ref="search" v-show="keyword">
+    <ul>
+      <li class="search-item border-bottom"
+      v-for="item of list" :key="item.id">
+        {{item.name}}
+      </li>
+
+      <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+    </ul>
+  </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      // 函数节流优化
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) === 0 ||
+           value.name.indexOf(this.keyword) === 0) {
+              result.push(value)
+            }
+          })
+        }
+        // 是一个对象数组
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    // 实现搜索下拉列表滚动
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 
 </script>
@@ -23,5 +81,19 @@ export default {
       line-height .62rem
       text-align center
       border-radius .06rem
+      color #666
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eee
+    .search-item
+      line-height .62rem
+      padding-left .2rem
+      background #fff
       color #666
 </style>
