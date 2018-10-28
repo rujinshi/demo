a7a85d2c990c0c1898a2755c2e37fc4bea959278
rujin_shi <rujin_shi@163.com>
2018-10-28 20:22:15 +0800

    finish pages

--- 5.travel/src/App.vue
+++ 5.travel/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <router-view/>
+    <!-- 会缓存不活动的组件实例 -->
+    <!-- 当组件在 <keep-alive> 内被切换，它的 activated 和 deactivated
+    这两个生命周期钩子函数将会被对应执行 -->
+    <keep-alive>
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
--- 5.travel/src/pages/home/Home.vue
+++ 5.travel/src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -25,15 +26,19 @@ export default {
   },
   data () {
     return {
+      lastCity: [],
       swiperList: [],
       iconList: [],
       recommendList: [],
       weekendList: []
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (res) {
@@ -47,8 +52,19 @@ export default {
       }
     }
   },
+  // 页面初次加载时
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  // 当组件在 <keep-alive> 内被切换，它的 activated
+  // 和 deactivated 这两个生命周期钩子函数将会被对应执行
+  activated () {
+    // 只有当上一次的城市和这一次城市不相等才再次发送ajax请求
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
